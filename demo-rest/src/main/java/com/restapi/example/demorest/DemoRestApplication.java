0548abf3f578b2c14397eaad4f6d915edf4c304c
Monika Sharma <monic.sh@gmail.com>
2019-12-07 15:16:21 -0800

    restful api demo

---
+++ demo-rest/src/main/java/com/restapi/example/demorest/DemoRestApplication.java
@@ -0,0 +1,16 @@
+package com.restapi.example.demorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApplication.class, args);
+		
+	}
+
+}
+
+
---
+++ demo-rest/src/main/java/com/restapi/example/demorest/Student.java
@@ -0,0 +1,64 @@
+package com.restapi.example.demorest;
+
+/*
+ {
+ "fname" : "monika",
+ "lname" : "sharma",
+ "score" : 69
+ }
+ */
+
+public class Student {
+	String fname;
+	String lname;
+	int score;
+
+	
+
+	public Student(String fname, String lname, int score) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.score = score;
+	}
+
+
+
+	public String getFname() {
+		return fname;
+	}
+
+
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+
+
+	public String getLname() {
+		return lname;
+	}
+
+
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+
+
+	public int getScore() {
+		return score;
+	}
+
+
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	
+	
+
+}
